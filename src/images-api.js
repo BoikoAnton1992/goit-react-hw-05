66f16319b4caede103c5a2ea36db7a275299c065
BoikoAnton1992 <145497164+BoikoAnton1992@users.noreply.github.com>
2024-04-29 17:08:35 +0300

    add api and start react-hw-05

--- src/images-api.js
+++ src/images-api.js
@@ -1,16 +1,56 @@
 import axios from 'axios';
 
-const apiKey = 'wbD1lFBCc3Aa77TKN4rbKXrhSZIa2Rqn48_IfhsccIo';
-const baseUrl = 'https://api.unsplash.com';
+const apiToken =
+  'eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJkNzk1OTgxYWQ0YzA0Yzg4NzVmOGQ3ZmRkNmQ3NzU3MiIsInN1YiI6IjY2MmY3YjgwZTMzZjgzMDEyYjIyMWYxYSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.BqVUp5iVEmJHNOa1Vd7RhKG4dG6bxg2p9AylwuZOb_M';
 
-export const fetchImages = async (query, page) => {
-  try {
-    const response = await axios.get(
-      `${baseUrl}/search/photos/?per_page=8&query=${query}&page=${page}&client_id=${apiKey}`
-    );
-    return response.data.results;
-  } catch (error) {
-    console.log('Error fetching images:', error);
-    throw error;
-  }
+axios.defaults.baseURL = 'https://api.themoviedb.org';
+axios.interceptors.request.use(config => {
+  config.headers.Authorization = `Bearer ${apiToken}`;
+  return config;
+});
+
+export const getMovies = async () => {
+  const response = await axios.get('3/trending/movie/day', {
+    params: {
+      language: 'en-US',
+    },
+  });
+  return response.data.results;
+};
+
+export const getMoviesSearch = async searchQuery => {
+  const response = await axios.get('3/search/movie', {
+    params: {
+      query: searchQuery,
+      language: 'en-US',
+    },
+  });
+  return response.data.results;
+};
+
+export const getMovieId = async movieId => {
+  const response = await axios.get(`3/movie/${movieId}`, {
+    params: {
+      language: 'en-US',
+    },
+  });
+  return response.data;
+};
+
+export const getMovieIdCredits = async movieId => {
+  const response = await axios.get(`3/movie/${movieId}/credits`, {
+    params: {
+      language: 'en-US',
+    },
+  });
+  return response.data;
+};
+
+export const getMovieIdReviews = async movieId => {
+  const response = await axios.get(`3/movie/${movieId}/reviews`, {
+    params: {
+      language: 'en-US',
+    },
+  });
+  return response.data;
 };
