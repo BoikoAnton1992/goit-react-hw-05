1bb56c5ed2dca804a08731e502b7e053b94f33de
BoikoAnton1992 <145497164+BoikoAnton1992@users.noreply.github.com>
2024-05-20 14:49:54 +0300

    Add useRef

--- src/pages/MovieDetailsPage/MovieDetailsPage.jsx
+++ src/pages/MovieDetailsPage/MovieDetailsPage.jsx
@@ -1,4 +1,4 @@
-import { Suspense, useEffect, useState } from 'react';
+import { Suspense, useEffect, useState, useRef } from 'react';
 import { Outlet, useParams } from 'react-router-dom';
 import { getMovieId } from '../../movies-api';
 import { Link } from 'react-router-dom';
@@ -13,7 +13,7 @@ export default function MovieDetailsPage() {
   const [movie, setMovie] = useState(null);
 
   const location = useLocation();
-  const backLinkHref = location.state ?? '/movies';
+  const backLinkHrefRef = useRef(location.state) ?? '/movies';
 
   useEffect(() => {
     async function fetchMovieInfo() {
@@ -32,7 +32,7 @@ export default function MovieDetailsPage() {
 
   return (
     <>
-      <Link className={css.link} to={backLinkHref}>
+      <Link className={css.link} to={backLinkHrefRef.current}>
         Go back
       </Link>
 
